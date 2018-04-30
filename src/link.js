ce8561a40bba70a18e1b18a2991cc588262f4584
Isa <lajansa@hotmail.com>
2018-04-30 15:31:19 +0800

    changed to ollida-api endpoint

--- src/link.js
+++ src/link.js
@@ -1,2 +1,2 @@
-export const baseLink = 'http://localhost:3001'
-// export const base_link = 'https://ollida-herokuapp'
+// export const baseLink = 'http://localhost:3001'
+export const baseLink = 'https://ollida-api.herokuapp.com'
